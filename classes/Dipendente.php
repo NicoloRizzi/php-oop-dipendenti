2018df93afeb60b627ceb3aa713b79365f123513
Nicolo <nicolorizzi1996@gmail.com>
2020-06-08 16:51:19 +0200

    CREATE INSTANCE

---
+++ classes/Dipendente.php
@@ -0,0 +1,24 @@
+<?php
+include_once __DIR__ . '/Persona.php';
+class Dipendente extends Persona {
+    //Properties
+    public $codMatricola;
+    public $mansione;
+
+    //Construct
+    public function __construct($_nome, $_cognome, $_eta, $_codMatricola, $_mansione) {
+        parent::__construct($_nome, $_cognome, $_eta);
+        $this->codMatricola = $_codMatricola;
+        $this->mansione = $_mansione;
+    }
+}
+$dipendenti = [
+    ['Mario', 'Rossi', rand(18, 65), 'D0001', 'Operaio'],
+    ['Pippo', 'Duzioni', rand(18, 65), 'D0002', 'Saldatore'],
+    ['Pluto', 'Duzioni', rand(18, 65), 'D0003', 'Autista']
+];
+$result = [];
+foreach ($dirigenti as $dirigente) {
+    $result[] = new Dirigente(...$dirigente);
+}
+?>
--- classes/Dirigente.php
+++ classes/Dirigente.php
@@ -1,6 +1,25 @@
 <?php
 include_once __DIR__ . '/Persona.php';
 class Dirigente extends Persona {
+    //Properties
+    public $area;
+    public $esperienza;
+
+    //Construct
+    public function __construct($_nome, $_cognome, $_eta, $_area, $_esperienza){
+        parent::__construct($_nome, $_cognome, $_eta);
+        $this->area = $_area;
+        $this->esperienza = $_esperienza;
+    }
     
+}
+$dirigenti = [
+    ['Mark', 'Zuckerberg', rand(18, 65), 'CEO', rand()],
+    ['Elon', 'Musk', rand(18, 65), rand()],
+    ['Jeff', 'Bezos', rand(18, 65), rand()]
+];
+$result = [];
+foreach ($dirigenti as $dirigente) {
+    $result[] = new Dirigente(...$dirigente);
 }
 ?>
